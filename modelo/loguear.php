dc531e2c382ee6b4ab301dc0709fef9d242c4b59
SebasF10 <sebastianfeo@colegioguanenta.edu.co>
2025-04-29 09:39:08 -0500

    se realizo el trabajo

---
+++ modelo/loguear.php
@@ -0,0 +1,23 @@
+<?php
+    require "conexcion.php";
+
+    $usuario = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query_1 = "SELECT email, COUNT(*) AS contar FROM Usuario WHERE email = '$usuario' AND password = '$password'";
+
+    $consulta = mysqli_query($conexion, $query_1) or trigger_error("Error en la consulta MYSQL: " + mysqli_error($conexion));
+
+    $resultado = mysqli_fetch_array($consulta);
+
+    if($resultado['contar'] > 0)
+    {
+        echo "El usuario existe en la BD <br>";
+        echo $resultado ['email'];
+    }
+    else
+    {
+        echo "El usuario no existe, o hay un error en el nombre de usuario o la contraseña";
+    }
+?>
+
