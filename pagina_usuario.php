e8f203eb80736c42ee511eea8fead2b77e425828
Sebastian feo Murillo <158294033+SebasF10@users.noreply.github.com>
2025-05-21 18:20:54 -0500

    Add files via upload

--- pagina_usuario.php
+++ pagina_usuario.php
@@ -25,6 +25,12 @@
     <hr>
     <a href="registrar_empleado.php">Registar empleado</a>
     <hr>
+    <h2> Consultas <h2>
+    <hr>
+    <a href=modelo\consultas_empleados.php>Empleados</a>
+    <hr>
+    <a href=#>Departamentos</a>
+    <hr>
     <a href="modelo/cerrar_sesion.php">Cerrar Sesion</a>
 
 </body>
